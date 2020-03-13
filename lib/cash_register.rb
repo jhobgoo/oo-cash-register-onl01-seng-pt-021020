b4034f4d0db39eda0571d8b9d4720d9ad0fa0ada
Joel Hobgood <jhobgoo@gmail.com>
2020-03-13 14:57:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,24 +22,19 @@ class CashRegister
     end
   end
   
-  def apply_discount
-    if @discount > 0
-      @discounted_amount = (price * discount)/100
-      @total -= @discounted_amount
-      return "After the discount, the total comes to $#{@total}."
+ def apply_discount
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
     else
-      return "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
-      
-  def items
-    @items
-  end
-  
+
   def void_last_transaction
-    binding.pry
-    @total -= @price
+    self.total = self.total - self.last_transaction
   end
+end
   
 end
 
