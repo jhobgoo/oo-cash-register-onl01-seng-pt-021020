5c6cb73d99ec62e6ea15acad55e50dbfde62199d
Joel Hobgood <jhobgoo@gmail.com>
2020-03-13 14:16:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,11 +5,13 @@ class CashRegister
   attr_accessor :total, :discount, :price, :items
   
   def initialize(discount = 0)
-    @total = 0
+    @total = total
     @discount = discount
     @price = price
     @items = []
   end
+  
+  
   
 end
 
